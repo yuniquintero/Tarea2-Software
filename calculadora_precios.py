62d6bdd748f5f4021c6d1cb0dd2da39647bc11ac
David Cabeza <cabezadavide@gmail.com>
2018-05-10 18:35:01 -0400

    Añade archivo base de calculadora de precios

---
+++ calculadora_precios.py
@@ -0,0 +1,15 @@
+##############################################################################
+# Universidad Simon Bolivar
+# Departamento de Computacion y Tecnologia de la Informacion
+# CI3715: Ingenieria de Software I
+#
+# Suite de pruebas unitarias para calcularadora_precios.py
+# ci3715.py
+#
+# @author Yuni Quintero 14-10880
+# @author David Cabeza 13-10191
+#
+# Prof. Alfonso Reinoza
+# @version 1 05/18
+##############################################################################
+
